dfe248b8e46d48f255cc15b1f80002a2af5f4ce3
Enrisen <2019120183@ub.edu.bz>
2025-03-16 10:37:28 -0600

    First commit.

---
+++ cmd/web/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"html/template"
+	"log/slog"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type application struct {
+	addr          *string
+	logger        *slog.Logger
+	templateCache map[string]*template.Template
+}
+
+func main() {
+	addr := flag.String("addr", "", "HTTP network address")
+	dsn := flag.String("dsn", "", "PostgreSQL DSN")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("database connection pool established")
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		addr:          addr,
+		logger:        logger,
+		templateCache: templateCache,
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
